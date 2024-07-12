46de8bc826b813581847405cb7c8fa5a99211fa1
Camilo Aguilera <camilo9497@live.com>
2024-07-12 17:39:03 -0500

    go to detail screen

--- components/CardMusic/index.tsx
+++ components/CardMusic/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
 import {TrackFlatList} from '../../types.ts/types';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   song: TrackFlatList;
@@ -9,9 +10,13 @@ interface Props {
 
 const CardMusic: React.FC<Props> = ({song}) => {
   const {item} = song;
+  const navigation = useNavigation();
+  const handlePress = () => {
+    navigation.navigate('SongDetailScreen', {item});
+  };
 
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
       <View style={styles.containerImage}>
         <Image style={styles.image} source={{uri: item.image[0]['#text']}} />
       </View>
