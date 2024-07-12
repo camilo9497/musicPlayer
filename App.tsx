52d6332ffe9b8b21fe5895b6dc704b63c108761f
Camilo Aguilera <camilo9497@live.com>
2024-07-12 11:25:58 -0500

    navigation settings and tabbar styles

--- App.tsx
+++ App.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import {colors} from './utils/colors';
+import {StatusBar} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Inicio" component={HomeScreen} />
-        <Tab.Screen name="Perfil" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <StatusBar barStyle="light-content" backgroundColor="#1E90FF" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: {backgroundColor: '#0e1726'},
+            tabBarActiveTintColor: colors.white,
+            tabBarInactiveTintColor: colors.gray,
+          }}>
+          <Tab.Screen
+            name="Inicio"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Perfil"
+            component={ProfileScreen}
+            options={{headerShown: false}}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
