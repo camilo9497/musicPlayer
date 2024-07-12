59896cf0375bf90c5a4cba0cfd82cf9dcfd70a4f
Camilo Aguilera <camilo9497@live.com>
2024-07-12 16:24:22 -0500

    Add error and load components

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {styles} from './styles';
 import Background from '../../components/Background';
 import CardList from '../../components/CardList';
 import useFetch from '../../hooks/useFetch';
 import {Songs} from '../../types.ts/types';
+import Error from '../../components/Error';
+import Load from '../../components/Load';
 
-const NUM_SONGS = 10;
+const NUM_SONGS = 20;
 const COUNTRY = 'colombia';
 const API_KEY = 'c19c47264b0dfd0973d63aa54cb6788c';
 const URL_API = `https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${COUNTRY}&api_key=${API_KEY}&format=json&limit=${NUM_SONGS}`;
@@ -15,30 +17,16 @@ function HomeScreen() {
   const {data, error, isLoading} = useFetch<Songs>(URL_API);
 
   if (isLoading) {
-    return (
-      <Background>
-        <Text style={styles.title}>Top 20 Colombia</Text>
-        <View style={{height: '100%', marginTop: 40}}>
-          <ActivityIndicator size={'large'} color={'white'} />
-        </View>
-      </Background>
-    );
+    return <Load />;
   }
 
   if (error) {
-    return (
-      <Background>
-        <Text style={styles.title}>Top 20 Colombia</Text>
-        <View style={{height: '100%', marginTop: 40}}>
-          <Text>Opps...se ha presentado un error</Text>
-        </View>
-      </Background>
-    );
+    return <Error />;
   }
 
   return (
     <Background>
-      <Text style={styles.title}>Top 20 Colombia</Text>
+      <Text style={styles.title}>{`Top ${NUM_SONGS} ${COUNTRY}`}</Text>
       <View style={styles.constainerList}>
         {data && <CardList songs={data} />}
       </View>
